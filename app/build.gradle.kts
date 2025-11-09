6379a83339276f0ef004d78a693b73f77aee9ad4
tymwitko <tymon.kobylecki@gmail.com>
2025-11-09 13:21:15 +0100

    remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.fragment)
     implementation(libs.androidx.runtime.android)
     implementation(libs.androidx.material3.android)
     implementation(libs.androidx.activity.compose)
