181441d2e2637d7dcf90ee62b557ac3f2dc31c0d
tymwitko <tymon.kobylecki@gmail.com>
2026-03-19 23:55:28 +0100

    fix crash cause apps aren't parcelable

--- app/src/main/java/com/tymwitko/recents/recentapps/RecentAppsActivity.kt
+++ app/src/main/java/com/tymwitko/recents/recentapps/RecentAppsActivity.kt
@@ -25,7 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -83,7 +83,7 @@ class RecentAppsActivity : AppCompatActivity() {
           .navigationBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally
       ) {
-        var appList: List<App> by rememberSaveable { mutableStateOf(allApps) }
+        var appList: List<App> by remember { mutableStateOf(allApps) }
         if (firstRun) {
           updateList()
           Log.d("TAG", "setting listener")
