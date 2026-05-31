fbfef3a1c390e371749c4498358b51b6baa72f83
tymwitko <tymon.kobylecki@gmail.com>
2026-05-31 22:08:25 +0200

    bump up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tymwitko.recents"
         minSdk = 25
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.4.1"
+        versionCode = 20
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
