e6c8a498ff501e9d7dc364cfc124984014b8df4f
tymwitko <tymon.kobylecki@gmail.com>
2026-08-01 16:48:55 +0200

    add version number on the bottom of settings

--- app/src/main/java/com/tymwitko/recents/settings/menu/SettingsMenuScreen.kt
+++ app/src/main/java/com/tymwitko/recents/settings/menu/SettingsMenuScreen.kt
@@ -4,15 +4,18 @@ import android.content.Intent
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.tymwitko.recents.BuildConfig
 import com.tymwitko.recents.recentapps.RecentAppsActivity
 import com.tymwitko.recents.settings.whitelist.SettingsViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -31,20 +34,24 @@ fun SettingsMenuScreen(
   Column(
     modifier = modifier
       .statusBarsPadding()
-      .navigationBarsPadding(),
+      .navigationBarsPadding()
+      .fillMaxSize(),
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
-      Box(modifier = Modifier
-        .fillMaxSize()
+    Box(
+      modifier = Modifier
         .weight(1f)
-      ) {
-        SettingsList(
-          modifier = Modifier
-            .fillMaxHeight(),
-          navController = navController,
-          entryNames = entryNames,
-          fontSize = viewModel.getFontSize()
-        )
-      }
+    ) {
+      SettingsList(
+        navController = navController,
+        entryNames = entryNames,
+        fontSize = viewModel.getFontSize()
+      )
     }
+    Text(
+      text = "v${BuildConfig.VERSION_NAME}",
+      color = MaterialTheme.colorScheme.onBackground,
+      fontSize = 12.sp
+    )
   }
+}
